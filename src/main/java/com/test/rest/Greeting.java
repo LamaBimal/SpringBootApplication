9cad3ed647039d75c2974773692131086506e835
lamabimal <bimaltamang1234@gmail.com>
2017-11-04 22:45:00 +0545

    Spring Boot Basic Rest  Service

---
+++ src/main/java/com/test/rest/Greeting.java
@@ -0,0 +1,23 @@
+package com.test.rest;
+
+/**
+ * Created by bimal on 11/4/17.
+ */
+public class Greeting {
+
+    private final long id;
+    private final String content;
+
+    public Greeting(long id, String content) {
+        this.id = id;
+        this.content  = content;
+    }
+
+    public long getId() {
+        return this.id;
+    }
+
+    public String getContent() {
+        return this.content;
+    }
+}
